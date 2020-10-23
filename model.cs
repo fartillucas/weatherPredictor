1719afb44525ccc7c393e3a2ca0ac3df9f947ea9
michael vittus <fartillucas@gmail.com>
2020-10-23 10:02:12 +0200

    update

--- model.cs
+++ model.cs
@@ -132,7 +132,7 @@ namespace consoletester
             double[] mytemparray = (from x in localTempMeanList select x).ToArray();
 
             int days = 365;
-            md.ShowMultiStepForecast(mytemparray, multipleForecast, days);
+            //md.ShowMultiStepForecast(mytemparray, multipleForecast, days);
 
 
             forecast = inverse(mytemparray, forecast, 365);
